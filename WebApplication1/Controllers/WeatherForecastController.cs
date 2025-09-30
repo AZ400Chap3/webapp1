53178678f506a624f42576e94e5fc1491269bf86
mohansukumara <32303509+mohansukumara@users.noreply.github.com>
2025-09-30 17:43:36 -0500

    Update WeatherForecastController.cs

--- WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplication1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","New-weather Read", "How to compare", "My best version"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","New-weather Read", "How to compare", "My best version 22"
             
         };
 
